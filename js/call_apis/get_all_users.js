55e8fa63704a4c08d1c6996c068f3de8b561e0c0
TênMac <caoducmanh2411999@gmail.com>
2023-04-07 18:05:58 +0300

    user_table Work on Delete API -byMacMie

--- js/call_apis/get_all_users.js
+++ js/call_apis/get_all_users.js
@@ -1,20 +1,18 @@
 // Set some global variables
 import { token_admin, token_user } from './default_tokens.js'
+import { user_getAll_local, user_deleteByID_local } from './default_apis.js'
 let valid = true;
 
-(async function getAllUsersApi() {
-    // Get link and variables of Object
-    var urlParams = new URLSearchParams(window.location.search);
+function getAllUsersApi() {
+    let promise = axios({
+        url: user_getAll_local,
+        method: 'GET',
+        headers: {
+            'Authorization': 'Bearer ' + token_admin
+        }
+    })
 
-    try {
-        var res = await axios({
-            url: 'http://localhost:8080/user/getAll',
-            method: 'GET',
-            headers: {
-                'Authorization': 'Bearer ' + token_admin
-            }
-        });
-
+    promise.then(function (res) {
         // Handle if successfully get data
         let arrayUsers = res.data;
         console.log(res.data);
@@ -22,10 +20,39 @@ let valid = true;
         // Render products array to layout
         renderUsersToTableUsers(arrayUsers);
         $('#example').DataTable();
-    } catch (e) {
+    })
+
+    promise.catch(function (err) {
+        // Handle if failed
         console.log(err);
+    })
+};
+
+// Automatically call APIs when page is loaded
+getAllUsersApi();
+
+// Function delete user
+const deleteUserFunction = (e) => {
+    let idUser = e.currentTarget.getAttribute("data-idUser");
+    let confirmation = confirm("Do you want to delete this User?");
+    if (confirmation) {
+        axios({
+            url: user_deleteByID_local + idUser,
+            method: 'DELETE',
+            headers: {
+                'Authorization': 'Bearer ' + token_admin
+            }
+        })
+            .then(function (response) {
+                alert("Delete successfully!");
+                getAllUsersApi();
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
     }
-})();
+}
+window.deleteUserFunction = deleteUserFunction;
 
 // Functions Render HTML
 function renderUsersToTableUsers(arrayUsers) {
@@ -44,7 +71,7 @@ function renderUsersToTableUsers(arrayUsers) {
             <td>
                 <a href="#" class="btn btn-sm btn-primary">Edit</a>
                 <a href="user-details.html" class="btn btn-sm btn-info">Detail</a>
-                <a href="#" class="btn btn-sm btn-danger disabled" title="Can't Delete Admin">Delete</a>
+                <a href="#" class="btn btn-sm btn-danger disabled" id="delete_user_button" data-idUser="${user.idUser}" onclick="deleteUserFunction(event)" title="Can't Delete Admin">Delete</a>
             </td>
         </tr>
             `;
@@ -60,7 +87,7 @@ function renderUsersToTableUsers(arrayUsers) {
             <td>
                 <a href="#" class="btn btn-sm btn-primary">Edit</a>
                 <a href="user-details.html" class="btn btn-sm btn-info">Detail</a>
-                <a href="#" class="btn btn-sm btn-danger" onclick="deleteUser(${user.idUser})">Delete</a>
+                <a href="#" class="btn btn-sm btn-danger" id="delete_user_button" data-idUser="${user.idUser}" onclick="deleteUserFunction(event)" >Delete</a>
             </td>
         </tr>
             `;
@@ -68,32 +95,12 @@ function renderUsersToTableUsers(arrayUsers) {
 
     }
     document.getElementById('list_users').innerHTML = contentHTML;
-}
-
-function deleteUser(userId) {
-    let confirmation = confirm("Bạn có chắc chắn muốn xóa người dùng này không?");
-    if (confirmation) {
-        axios({
-            url: 'http://localhost:8080/user/delete/' + userId,
-            method: 'DELETE',
-            headers: {
-                'Authorization': 'Bearer ' + token_admin
-            }
-        })
-            .then(function (response) {
-                alert("Xóa người dùng thành công!");
-                getAllUsersApi();
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-    }
-}
+};
 
 // Call API to get array Users
 // (function getAllUsersApi() {
 //     let promise = axios({
-//         url: 'http://localhost:8080/user/getAll',
+//         url: user_getAll_local,
 //         method: 'GET',
 //         headers: {
 //             'Authorization': 'Bearer ' + token_admin
@@ -107,6 +114,7 @@ function deleteUser(userId) {
 
 //         // Render products array to layout
 //         renderUsersToTableUsers(arrayUsers);
+//         $('#example').DataTable();
 //     })
 
 //     promise.catch(function (err) {
@@ -114,6 +122,52 @@ function deleteUser(userId) {
 //         console.log(err);
 //     })
 // })();
+
+// Automatically call APIs when page is loaded
+// (async function getAllUsersApi() {
+//     // Get link and variables of Object
+//     var urlParams = new URLSearchParams(window.location.search);
+
+//     try {
+//         var res = await axios({
+//             url: 'http://localhost:8080/user/getAll',
+//             method: 'GET',
+//             headers: {
+//                 'Authorization': 'Bearer ' + token_admin
+//             }
+//         });
+
+//         // Handle if successfully get data
+//         let arrayUsers = res.data;
+//         console.log(res.data);
+
+//         // Render products array to layout
+//         renderUsersToTableUsers(arrayUsers);
+//         $('#example').DataTable();
+//     } catch (e) {
+//         console.log(err);
+//     }
+// })();
+
+// function deleteUserFunction(userId) {
+//     let confirmation = confirm("Bạn có chắc chắn muốn xóa người dùng này không?");
+//     if (confirmation) {
+//         axios({
+//             url: 'http://localhost:8080/user/delete/' + userId,
+//             method: 'DELETE',
+//             headers: {
+//                 'Authorization': 'Bearer ' + token_admin
+//             }
+//         })
+//             .then(function (response) {
+//                 alert("Xóa người dùng thành công!");
+//                 getAllUsersApi();
+//             })
+//             .catch(function (error) {
+//                 console.log(error);
+//             });
+//     }
+// }
 
 // function renderUsersToTableUsers(arrayUsers) {
 //     let contentHTML = `
